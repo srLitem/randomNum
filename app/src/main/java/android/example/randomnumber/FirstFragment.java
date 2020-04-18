92768ca616209794d5e45f4b2543bc0f52441d3c
Santiago Rhenals <santiagorhenals@gmail.com>
2020-04-18 18:39:59 +0200

    Allow users to increase the number by 1

---
+++ app/src/main/java/android/example/randomnumber/FirstFragment.java
@@ -0,0 +1,65 @@
+package android.example.randomnumber;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+    TextView showCountTextView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        // Find the text view with the number
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+        return fragmentFirstLayout;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast myToast = Toast.makeText(getActivity(), R.string.toast_message_text, Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                countMe(view);
+            }
+        });
+    }
+
+    private void countMe(View view){
+        // Get the text view value
+        String countString = showCountTextView.getText().toString();
+        // Convert it to an integer and increase it by 1
+        Integer count = Integer.parseInt(countString);
+        count++;
+        // Update the value of the Text view
+        showCountTextView.setText(count.toString());
+    }
+}
